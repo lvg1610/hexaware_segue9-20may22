9411e8e6d503884474c7ab86d5ed2dc4e9ea0977
lvg1610 <lvg1610@gmail.com>
2022-05-20 12:14:01 +0530

    v1 added Employee.java ,Student.java

---
+++ Employee.java
@@ -0,0 +1,15 @@
+public class Employee
+{
+    private int employeeId;
+    private String employeeName;
+    private double salary;
+    private int departmentId;
+
+    public Employee()
+    {
+    }
+
+    public boolean equals(Object obj)
+    {
+    } 
+}
---
+++ Studuent.java
@@ -0,0 +1,16 @@
+public class Student
+{
+    private int rollNo;
+    private String studentName;
+    private String[] subjects;
+    private LocalDate dateOfBirth;
+
+    public Student()
+    {
+    }
+   
+    public String toString()
+    {
+    }
+
+} 
